1476de59fb9b7e9528a8d076c467b6123baca662
Matthew Jacques <iFtw@live.co.uk>
2025-06-09 22:59:06 +0100

    WIP

--- lib/http_logger.rb
+++ lib/http_logger.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
+require_relative 'http_logger/http_logger'
+require_relative 'http_logger/net_http'
 require_relative 'http_logger/version'
-
-module HttpLogger
-  class Error < StandardError; end
-  # Your code goes here...
-end
